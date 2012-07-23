38b875dbc3fbf659acc44c7c7fa219ef34e331d8
devbootcamp <apprentice@devbootcamp.com>
2012-07-23 10:38:43 -0700

    create a new app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HnClone::Application.config.secret_token = 'dc89d37f3bf1483cf9193f4bf1fa9e3050ce47839706926bba7e84dab30a5abef8128b6af8e493d8dff996ec00796f8af29c4d920f861a9dca46f1fdea870372'
