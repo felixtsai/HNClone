fcb467a0a5acd3fcabf4c3d975158edb87969a4b
Orasa Munmueang + Felix Tsai <apprentice+om+ft@devbootcamp.com>
2012-07-24 12:43:49 -0700

    user validation

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,19 +3,24 @@ class SessionsController < ApplicationController
   end
 
   def create
-  	user = User.authenticate(params[:email], params[:password])
-	if user
-		session[:user_id] = 5
-		redirect_to links_path, :notice => "Logged in!"
-	else
-		flash.now.alert = "Invalid email or passord"
-		render "new"
-	end #end if
+    user = User.authenticate(params[:email], params[:password])
+  if user
+    session[:user_id] = user.id
+    redirect_to links_path, :notice => "Logged in!"
+  else
+    flash.now.alert = "Invalid email or password"
+    render "new"
+  end #end if
   end #end create
 
   def destroy
-  	session[:user_id] = nil
-  	redirect_to links_path, :notice => "Logged out!"
+    session[:user_id] = nil
+    redirect_to new_session_path, :notice => "Logged out!"
   end
+
+  def show
+
+  end
+
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :password, :user_id
+  attr_accessible :email, :password, :id, :password_confirmation
 
   before_save { |user| user.email = email.downcase }
 
@@ -8,12 +8,12 @@ class User < ActiveRecord::Base
   validates :password, presence: true, length: {minimum: 6}
 
   def self.authenticate(email, password)
-  	user = find_by_email(email)
-  	if user && user.password
-  		user
-  	else
-  		nil
-  	end #end if
+    user = find_by_email(email)
+    if user && user.password == password
+      user
+    else
+      nil
+    end #end if
   end #end authenticate
 
 end
